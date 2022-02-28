0eeb40a9885f2264eeb4641dbeb5b34656596012
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-02-28 15:07:16 +0900

    Fix: prefixPath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+const prefixPath = isProd ? '/example-export-nextjs-sub-directory' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: prefixPath,
+  basePath: prefixPath,
   reactStrictMode: true,
 }
 
